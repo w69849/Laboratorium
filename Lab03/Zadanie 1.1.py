c600acd21e88ae21e945b656d4999c465229b0e2
w69849 <Michal.Soowa@onet.pl>
2023-11-26 14:59:05 +0100

    Zadanie 1.1

---
+++ Lab03/Zadanie 1.1.py
@@ -0,0 +1,15 @@
+list1 = ["Michał", "Paweł", "Piotr", "Bartek"]
+
+sorted_list1 = sorted(list1)
+
+sorted_list1.append("Anna")
+sorted_list1.append("Kasia")
+
+sorted_list1.pop()
+
+sorted_list1.insert(2, "Zosia")
+
+sorted_list1.reverse()
+
+sorted_list1 *= 2
+print(sorted_list1)
