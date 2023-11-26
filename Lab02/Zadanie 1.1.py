40de6d3d5c474067d862925dbe4063f4561a7913
w69849 <Michal.Soowa@onet.pl>
2023-11-26 14:43:53 +0100

    Zadanie 1.4

--- Lab02/Zadanie 1.1.py
+++ Lab02/Zadanie 1.1.py
@@ -3,7 +3,11 @@ y = int(input("Podaj liczbe1: "))
 
 if(x <= y):
     for i in range(x, y + 1):
+        if(i%2 != 0):
+            continue
         print(i)
 else:
     for i in range(y, x + 1):
+        if (i % 2 != 0):
+            continue
         print(i)
