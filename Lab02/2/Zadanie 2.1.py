1b34a4d330a069a92cf2743e0710afe97936e4db
w69849 <michael_sowa@tapoceans.com>
2023-12-07 01:16:24 +0100

    Lab02 - Zadanie 2.1

---
+++ Lab02/2/Zadanie 2.1.py
@@ -0,0 +1,11 @@
+for i in range(1, 101):
+    print(i)
+
+for i in reversed(range(0, 101)):
+    print(i)
+
+for i in range(7, 78, 7):
+    print(i)
+
+for i in reversed(range(0, 22, 2)):
+    print(i)
