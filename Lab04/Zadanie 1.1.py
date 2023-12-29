2c280b7c3ca9b906736dcdd67b89a5ac7c9357b1
w69849 <michael_sowa@tapoceans.com>
2023-12-29 15:23:51 +0100

    Lab04 - Zadanie 1.1 fix

--- Lab04/Zadanie 1.1.py
+++ Lab04/Zadanie 1.1.py
@@ -1,9 +1,14 @@
 def get_circle_area(r):
     if(r>0):
         return r*r*3.14
-    else:
-        print("Nie można obliczyć pola:")
 
-r = float(input("Podaj pole koła: "))
+try:
+    r = float(input("Podaj pole koła: "))
+
+    if(r <= 0):
+        raise ValueError
+except ValueError:
+    print("Nie ma koła o takim promieniu")
+    exit()
 
 print(get_circle_area(r))
