c89bf1ee4227f9ba298983503657b8cef3f9446f
w69849 <michael_sowa@tapoceans.com>
2024-01-25 18:19:52 +0100

    Lab05 - Zadanie 1.5

---
+++ Lab05/Zadanie 1.5.py
@@ -0,0 +1,7 @@
+import keyword
+
+print("for: ", keyword.iskeyword("for"))
+print("print: ", keyword.iskeyword("print"))
+print("break: ", keyword.iskeyword("break"))
+print("done: ", keyword.iskeyword("done"))
+print("bad: ", keyword.iskeyword("bad"))
